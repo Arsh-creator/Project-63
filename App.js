bb5a9b71f43d7d2313db22a88e15c9fd8ef5d5f7
Arsh-creator <75788096+Arsh-creator@users.noreply.github.com>
2021-06-24 07:53:16 -0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import homeScreen from './homeScreen';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import Read from './screens/Read';
+import Write from './screens/Write'
 
-export default function App() {
+export default class App extends React.Component{
+  render(){
   return (
-    <View>
-      <homeScreen />
-    </View>
+    <AppContainer/>
   );
 }
+}
+
+const TabNavigator = createBottomTabNavigator({
+  ReadStory: {screen: Read},
+  WriteStory: {screen: Write},
+},
+{defaultNavigationOptions: ({navigation})=>({
+  tabBarIcon: ({})=>{
+    const routeName = navigation.state.routeName
+    if(routeName === 'ReadStory'){
+      return(
+        <Image 
+        source={require('./read.png')} />
+      );
+    } else if(routeName === 'WriteStory'){
+      return(
+        <Image 
+        source={require("./write.png")} />
+      );
+    }
+  }
+})}
+);
+
+const AppContainer = createAppContainer(TabNavigator);
